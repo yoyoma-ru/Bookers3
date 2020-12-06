0fdd34f2dc11194fa833ce886929da8a5fd3af4c
愿山聖 <asjstksmr@gmail.com>
2020-12-06 11:44:52 +0900

    [fin]dmm-application_plactice1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book), notice: "Book was successfully created."
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book), notice: "Book was successfully updated."
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
